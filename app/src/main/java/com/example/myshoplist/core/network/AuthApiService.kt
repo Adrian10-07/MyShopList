628497222c088f06614ad58e995c24830e735634
Adrian10-07 <adrian2aguilarg@gmail.com>
2026-02-06 00:09:54 -0600

    login corregido

--- app/src/main/java/com/example/myshoplist/core/network/AuthApiService.kt
+++ app/src/main/java/com/example/myshoplist/core/network/AuthApiService.kt
@@ -11,4 +11,5 @@ interface AuthApiService {
     suspend fun login(@Body request: LoginRequest): Response<LoginResponse>
     @POST("auth/register")
     suspend fun register(@Body request: RegisterRequest): Response<LoginResponse>
+
 }
